331175ec3cf63c0fe22ad1475d7f36fb7eebe08d
BrunoSM <62313823+Bruno-Marian@users.noreply.github.com>
2023-11-07 20:02:32 -0300

    Subindo alterações

--- src/QuatroEstacoes/QuatroEstacoes/Constants.cs
+++ src/QuatroEstacoes/QuatroEstacoes/Constants.cs
@@ -2,9 +2,9 @@
 {
     public class Constants
     {
-        public const string UrlServer = "mqtt.sisgel.com";
-        public const string UserServer = "Bruno";
-        public const string PassServer = "123456";
-        public const int PortServer = 1883;
+        public const string UrlServer = "blu.sisgel.com";
+        public const string UserServer = "Senac";
+        public const string PassServer = "Senac";
+        public const int PortServer = 41883;
     }
 }
